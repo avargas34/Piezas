60565b8a06f0faad4b18cf567f897e5a93d3bc22
Anthony Vargas <Anthony@Anthonys-MacBook-Pro-6.local>
2020-05-06 22:57:48 -0700

    Added valid and invalid location tests

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,14 +4,14 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
-	protected:
-		PiezasTest(){} //constructor runs before each test
-		virtual ~PiezasTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+protected:
+	PiezasTest() {}			   //constructor runs before each test
+	virtual ~PiezasTest() {}   //destructor cleans up after tests
+	virtual void SetUp() {}	   //sets up before each test (after constructor)
+	virtual void TearDown() {} //clean up after each test, (before destructor)
 };
 
 TEST(PiezasTest, sanityCheck)
@@ -26,16 +26,112 @@ TEST(PiezasTest, blank_board_check)
 	bool blank_board = true;
 
 	for (auto i = 0; i < BOARD_ROWS; i++)
-    {
-        for (auto i2 = 0; i2 < BOARD_COLS; i2++)
-        {
-            if(connect4.pieceAt(i, i2) != blank)
+	{
+		for (auto i2 = 0; i2 < BOARD_COLS; i2++)
+		{
+			if (connect4.pieceAt(i, i2) != blank)
 			{
 				blank_board = false;
 				break;
 			}
-        }
-    }
+		}
+	}
+
+	EXPECT_EQ(blank_board, true);
+}
+
+TEST(PiezasTest, valid_location)
+{
+	Piezas connect4;
+	char blank = ' ';
+	bool board_piece = true;
+	int i = 0;
+	int i2 = 0;
+
+	if (connect4.pieceAt(i, i2) == blank)
+	{
+		board_piece = true;
+	}
+
+	EXPECT_EQ(blank_board, true);
+}
+
+TEST(PiezasTest, valid_location2)
+{
+	Piezas connect4;
+	char blank = ' ';
+	bool board_piece = true;
+	int i = 2;
+	int i2 = 0;
+
+	if (connect4.pieceAt(i, i2) == blank)
+	{
+		board_piece = true;
+	}
+
+	EXPECT_EQ(blank_board, true);
+}
+
+TEST(PiezasTest, valid_location3)
+{
+	Piezas connect4;
+	char blank = ' ';
+	bool board_piece = true;
+	int i = 2;
+	int i2 = 3;
+
+	if (connect4.pieceAt(i, i2) == blank)
+	{
+		board_piece = true;
+	}
+
+	EXPECT_EQ(blank_board, true);
+}
+
+TEST(PiezasTest, invalid_location)
+{
+	Piezas connect4;
+	char invalid = '?';
+	bool board_piece = true;
+	int i = 1;
+	int i2 = -4;
+
+	if (connect4.pieceAt(i, i2) == invalid)
+	{
+		board_piece = true;
+	}
+
+	EXPECT_EQ(blank_board, true);
+}
+
+TEST(PiezasTest, invalid_location2)
+{
+	Piezas connect4;
+	char invalid = '?';
+	bool board_piece = true;
+	int i = -1;
+	int i2 = 3;
+
+	if (connect4.pieceAt(i, i2) == invalid)
+	{
+		board_piece = true;
+	}
+
+	EXPECT_EQ(blank_board, true);
+}
+
+TEST(PiezasTest, invalid_location3)
+{
+	Piezas connect4;
+	char invalid = '?';
+	bool board_piece = true;
+	int i = -2;
+	int i2 = 8;
+
+	if (connect4.pieceAt(i, i2) == invalid)
+	{
+		board_piece = true;
+	}
 
 	EXPECT_EQ(blank_board, true);
 }
