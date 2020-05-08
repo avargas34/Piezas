b305c5dc3ec2c8d1b5bb2f37e852a504ce8ea6bf
Anthony Vargas <Anthony@Anthonys-MacBook-Pro-6.local>
2020-05-08 02:51:13 -0700

    Fixed broken test

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -530,16 +530,16 @@ TEST(PiezasTest, hori_game_X_win2)
 	bool game = false;
 
 	connect4.dropPiece(0);
+	connect4.dropPiece(0);
 	connect4.dropPiece(1);
+	connect4.dropPiece(0);
 	connect4.dropPiece(2);
-	connect4.dropPiece(3);
-	connect4.dropPiece(0);
-	connect4.dropPiece(0);
-	connect4.dropPiece(1);
-	connect4.dropPiece(1);
 	connect4.dropPiece(2);
 	connect4.dropPiece(3);
 	connect4.dropPiece(2);
+	connect4.dropPiece(1);
+	connect4.dropPiece(3);
+	connect4.dropPiece(1);
 	connect4.dropPiece(3);
 
 	if(connect4.gameState() == X)
